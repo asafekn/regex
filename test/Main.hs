df53dad9c5c9611a6f236aff325e2e6f9f36a142
Asafe Klain <asafeklain77@gmail.com>
2025-06-11 13:08:10 +0100

    Fix multi-result handling

--- test/Main.hs
+++ test/Main.hs
@@ -20,3 +20,66 @@ main = hspec $ do
 
     it "Plus does not match zero times" $
       evaluate (Plus (MatchChar 'a')) "b" `shouldBe` Nothing
+
+    it "Matches from the start" $
+      evaluate (And MatchStart (MatchChar 'a')) "ab" `shouldBe` Just "a"
+
+    it "MatchStart does not match after the start" $
+      evaluate (And MatchStart (MatchChar 'a')) "ba" `shouldBe` Nothing
+
+
+-- evaluate (Plus (MatchChar 'a')) "aaa"
+-- asum $ fmap (parse (regexParser (Plus (MatchChar 'a'))) $ tails "aaa"
+-- asum $ fmap (parse (regexParser (Plus (MatchChar 'a'))) $ ["aaa", "aa", "a"]
+-- parse (regexParser (Plus (MatchChar 'a')) "aaa"
+-- run (Plus (MatchChar 'a')) "aaa"
+--  (r, str') <- run (MatchChar 'a') "aaa"
+--    run (MatchChar 'a') "aaa"
+--  (r, str') <- [(['a'], "aa")]
+--  (['a'], "aa")
+--  (rs, str'') <- zeroOrMore (MatchChar 'a') "aa"
+--    zeroOrMore (MatchChar 'a') "aa"
+--      run (MatchChar 'a') "aa"
+--      [(['a'], ['a'])]
+--      (x, str') <- [(['a'], ['a'])]
+--      (y, str'') <- zeroOrMore (MatchChar 'a') ['a']
+--        zeroOrMore (MatchChar 'a') ['a']
+--          run (MatchChar 'a') ['a']
+--          [(['a'],[])]
+--          (x, str') <- [(['a'],[])]
+--          (y, str'') <- zeroOrMore (MatchChar 'a') []
+--            run (MatchChar 'a') []
+--            []
+--            [] -> [("", [])]
+--          (y, str'') <- [("", [])]
+--          ("", []) <- [("", [])]
+--          [("", ['a']), (['a'], [])]
+--      (y, str'') <- [("", ['a']), (['a'], [])]
+--        (y, str'') == ("", ['a'])
+--          [("", "aa"), (['a'], ['a'])]
+--        (y, str'') == (['a'], [])
+--          [("", "aa"), ( ['a'] <> ['a'], [])]
+--          [("", "aa"), ( ["aa"], [])]
+--      [("", "aa"), (['a'], ['a']), ("", "aa"), ( ["aa"], [])]
+--  (rs, str'') <- [("", "aa"), (['a'], ['a']), ("", "aa"), ( ["aa"], [])]
+--    (rs, str'') <- ("", "aa")
+--      return (['a'] <> [], "aa")
+--      [(['a'] <> [], "aa")]
+--      [(['a'], "aa")]
+--    (rs, str'') <- (['a'], ['a'])
+--      return (['a'] <> ['a'], ['a'])
+--      return ("aa", ['a'])
+--
+--    (rs, str'') <- ("", "aa")
+--      return (['a'], "aa")
+--
+--    (rs, str'') <- ( "aa", [])]
+--      return (['a'] <> "aa", [])
+--      return ("aaa", [])
+--  [(['a'], "aa"), ("aa", ['a']), (['a'], "aa"), ("aaa", [])]
+--
+--
+--
+--
+--
+
