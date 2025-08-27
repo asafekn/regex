296e5b6171509b4cf72af94003fbfdb7e21a7fa6
Marcelo Lazaroni <lazaronijunior@gmail.com>
2025-08-27 10:49:02 +0100

    Add evaluation tests for Or

--- test/Main.hs
+++ test/Main.hs
@@ -46,116 +46,6 @@ main = hspec $ do
       it "Or matches second alternative with two characters" $
         evaluate (Or (And (MatchChar 'a') (MatchChar 'b')) (And (MatchChar 'x') (MatchChar 'y'))) "xyc" `shouldBe` Just "xy"
 
-
--- Or matches first alternative with two characters
---
--- run isStart regex str
--- run True (Or (And (MatchChar 'a') (MatchChar 'b')) (And (MatchChar 'x') (MatchChar 'y'))) "abc"
--- 	Or x y ->
--- 	Or (And (MatchChar 'a') (MatchChar 'b')) (And (MatchChar 'x') (MatchChar 'y'))) ->
--- 		case run isStart x str of
--- 		case run True (and (MatchChar 'a') (MatchChar 'b')) "abc" of
--- 			And x y -> do
--- 			And (MatchChar 'a') (MatchChar 'b') -> do
--- 		        	(r1, str') <- run isStart x str
--- 		        	(r1, str') <- run True (MatchChar 'a') "abc"
--- 		        		MatchChar c ->
--- 		        		MatchChar 'a' ->
--- 		        			case str of
--- 		        			case "abc" of
--- 		        				x : xs ->
--- 		        				"a" : "bc" ->
--- 		        					if x == c
--- 		        					if "a" == 'a'
--- 		        					then [([x], xs)]
--- 		        					then [(["a"], "bc")]
--- 		        	(["a"], "bc") <- run True (MatchChar 'a') "abc"
--- 		        	let isStart' = isStart && r1 == ""
--- 		        	let isStart' = True && ["a"] == ""
--- 		        	(r2, str'') <- run isStart' y str'
--- 		        	(r2, str'') <- run False (MatchChar 'b') "bc"
--- 		        		MatchChar c ->
--- 		        		MatchChar 'b' ->
--- 		        			case str of
--- 		        			case "bc" of
--- 		        				x : xs ->
--- 		        				"b" : "c" ->
--- 		        					if x == c
--- 		       						if "b" == 'b'
--- 		       						then [([x], xs)]
--- 		       						then [(["b"], "c")]
--- 		        	(["b"], "c") <- run False (MatchChar 'b') "bc"
--- 		        	return (r1 <> r2, str'')
--- 		        	return (["a"] <> ["b"], "c")
--- 		        	return (["ab"], "c")
--- 		case (["ab"], "c") of
--- 		r -> r
---		(["ab"], "c") -> (["ab"], "c")
---
---
---
--- Or matches second alternative with two characters
---
--- run isStart regex str
--- run True (Or (And (MatchChar 'a') (MatchChar 'b')) (And (MatchChar 'x') (MatchChar 'y'))) "xyc"
--- 	Or x y ->
--- 	Or (And (MatchChar 'a') (MatchChar 'b')) (And (MatchChar 'x') (MatchChar 'y'))) ->
--- 		case run isStart x str of
--- 		case run True (and (MatchChar 'a') (MatchChar 'b')) "xyc" of
--- 			And x y -> do
--- 			And (MatchChar 'a') (MatchChar 'b') -> do
--- 		        	(r1, str') <- run isStart x str
--- 		        	(r1, str') <- run True (MatchChar 'a') "xyc"
--- 		        		MatchChar c ->
--- 		        		MatchChar 'a' ->
--- 		        			case str of
--- 		        			case "xyc" of
--- 		        				x : xs ->
--- 		        				"x" : "yc" ->
--- 		        					if x == c
--- 		        					if "x" == 'a'
--- 		        					else []
--- 		        	[] <- run True (MatchChar 'a') "abc"
--- 		case [] of
---		[] -> run isStart y str
---		[] -> run True (And (MatchChar 'x') (MatchChar 'y')) "xyc"
---			And x y -> do
--- 			And (MatchChar 'x') (MatchChar 'y') -> do
--- 		        	(r1, str') <- run isStart x str
--- 		        	(r1, str') <- run True (MatchChar 'x') "xyc"
--- 		        		MatchChar c ->
--- 		        		MatchChar 'x' ->
--- 		        			case str of
--- 		        			case "xyc" of
--- 		        				x : xs ->
--- 		        				"x" : "yc" ->
--- 		        					if x == c
--- 		        					if "x" == 'x'
--- 		        					then [([x], xs)]
--- 		        					then [(["x"], "yc")]
--- 		        	(["x"], "yc") <- run True (MatchChar 'x') "xyc"
--- 		        	let isStart' = isStart && r1 == ""
--- 		        	let isStart' = True && ["x"] == ""
--- 		        	(r2, str'') <- run isStart' y str'
--- 		        	(r2, str'') <- run False (MatchChar 'y') "yc"
--- 		        		MatchChar c ->
--- 		        		MatchChar 'y' ->
--- 		        			case str of
--- 		        			case "yc" of
--- 		        				x : xs ->
--- 		        				"y" : "c" ->
--- 		        					if x == c
--- 		       						if "y" == 'y'
--- 		       						then [([x], xs)]
--- 		       						then [(["y"], "c")]
--- 		        	(["y"], "c") <- run False (MatchChar 'y') "yc"
--- 		        	return (r1 <> r2, str'')
--- 		        	return (["x"] <> ["y"], "c")
--- 		        	return (["xy"], "c")
---
---
-
-
     describe "compile" $ do
       it "MatchStart" $
         compile "^" `shouldBe` MatchStart
@@ -256,68 +146,14 @@ main = hspec $ do
       it "Test escaping" $
         evaluate (compile "a\\?b") "ca?bc" `shouldBe` Just "a?b"
 
+      it "Or" $
+        evaluate (compile "a|b") "b" `shouldBe` Just "b"
 
+      it "Or with more alternatives" $
+        evaluate (compile "a|b|c") "abc" `shouldBe` Just "a"
 
+      it "Or with Ands" $
+        evaluate (compile "ab|cd") "cd" `shouldBe` Just "cd"
 
-
-
-
-
-
---     compile "$" == MatchEnd
---     compile "a+" == Plus (MatchChar 'a')
---     compile "a*" == Asterisk (MatchChar 'a')
---     compile "aba*d+" == ....
-
-
--- evaluate (Plus (MatchChar 'a')) "aaa"
--- asum $ fmap (parse (regexParser (Plus (MatchChar 'a'))) $ tails "aaa"
--- asum $ fmap (parse (regexParser (Plus (MatchChar 'a'))) $ ["aaa", "aa", "a"]
--- parse (regexParser (Plus (MatchChar 'a')) "aaa"
--- run (Plus (MatchChar 'a')) "aaa"
---  (r, str') <- run (MatchChar 'a') "aaa"
---    run (MatchChar 'a') "aaa"
---  (r, str') <- [(['a'], "aa")]
---  (['a'], "aa")
---  (rs, str'') <- zeroOrMore (MatchChar 'a') "aa"
---    zeroOrMore (MatchChar 'a') "aa"
---      run (MatchChar 'a') "aa"
---      [(['a'], ['a'])]
---      (x, str') <- [(['a'], ['a'])]
---      (y, str'') <- zeroOrMore (MatchChar 'a') ['a']
---        zeroOrMore (MatchChar 'a') ['a']
---          run (MatchChar 'a') ['a']
---          [(['a'],[])]
---          (x, str') <- [(['a'],[])]
---          (y, str'') <- zeroOrMore (MatchChar 'a') []
---            run (MatchChar 'a') []
---            []
---            [] -> [("", [])]
---          (y, str'') <- [("", [])]
---          ("", []) <- [("", [])]
---          [("", ['a']), (['a'], [])]
---      (y, str'') <- [("", ['a']), (['a'], [])]
---        (y, str'') == ("", ['a'])
---          [("", "aa"), (['a'], ['a'])]
---        (y, str'') == (['a'], [])
---          [("", "aa"), ( ['a'] <> ['a'], [])]
---          [("", "aa"), ( ["aa"], [])]
---      [("", "aa"), (['a'], ['a']), ("", "aa"), ( ["aa"], [])]
---  (rs, str'') <- [("", "aa"), (['a'], ['a']), ("", "aa"), ( ["aa"], [])]
---    (rs, str'') <- ("", "aa")
---      return (['a'] <> [], "aa")
---      [(['a'] <> [], "aa")]
---      [(['a'], "aa")]
---    (rs, str'') <- (['a'], ['a'])
---      return (['a'] <> ['a'], ['a'])
---      return ("aa", ['a'])
---
---    (rs, str'') <- ("", "aa")
---      return (['a'], "aa")
---
---    (rs, str'') <- ( "aa", [])]
---      return (['a'] <> "aa", [])
---      return ("aaa", [])
---  [(['a'], "aa"), ("aa", ['a']), (['a'], "aa"), ("aaa", [])]
-
-
+      it "Or with parentesis" $
+        evaluate (compile "(a|b)c") "abcd" `shouldBe` Just "bc"
